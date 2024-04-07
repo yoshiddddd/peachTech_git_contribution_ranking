32a528b5a7f82bd258e0c958717a96b1166079e8
yoshiddddd <yoshidakerapo@gmail.com>
2024-04-07 15:57:02 +0900

    tab background changed

--- src/utils/CenterdTabs.js
+++ src/utils/CenterdTabs.js
@@ -5,6 +5,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import '../css/CenterdTabs.css'
+import { normalizeConfig } from '@apollo/client/cache/inmemory/helpers';
 const StyledPaper = styled(Paper)({
   flexGrow: 1,
 });
@@ -13,15 +14,20 @@ const StyledTabs = styled(Tabs)(({ theme }) => ({
   [theme.breakpoints.down('sm')]: { // 600px以下のデバイスに適用
     '& .MuiTabs-flexContainer': {
       flexDirection: 'column', // タブを縦に並べる
+        // display: 'none',
     },
   },
 }));
 
+
 const StyledTab = styled(Tab)(({ theme }) => ({
-  [theme.breakpoints.down('sm')]: {
-    minWidth: '100%', // タブの幅を100%にする
-  },
-}));
+    [theme.breakpoints.down('sm')]: {
+      minWidth: '100%',
+    },
+    '&.Mui-selected': { // 選択されたタブのスタイル
+      backgroundColor: '#d3d3d3', // ここに好みの色を設定
+    },
+  }));
 
 const TabPanel = (props) => {
   const { children, value, index, ...other } = props;
@@ -59,6 +65,7 @@ export const CenteredTabs = (props) => {
           indicatorColor="primary"
           textColor="primary"
           centered
+          TabIndicatorProps={{ style: { display: 'none' } }}
         >
           {props.labels.map((label, index) => <StyledTab key={index} label={label} className='StyledTab' />)} 
         </StyledTabs>
