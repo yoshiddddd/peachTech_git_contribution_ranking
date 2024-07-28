aa5ff415647e012a8a73b35be00d78ad8cc7ca45
yoshiddddd <yoshidakerapo@gmail.com>
2024-07-28 19:59:18 +0900

    add meta

--- src/utils/GetQuery.ts
+++ src/utils/GetQuery.ts
@@ -5,7 +5,6 @@ export const GET_CONTRIBUTIONS = gql`
   query UserInfo($login: String!, $from: DateTime!, $to: DateTime!) {
     user(login: $login) {
       login 
-      name
       avatarUrl
       totalContributionsCollection: contributionsCollection(from: "2024-01-01T00:00:00Z", to: "2024-12-31T23:59:59Z") {
         contributionCalendar {
@@ -18,12 +17,6 @@ export const GET_CONTRIBUTIONS = gql`
         }
       }
     }
-        rateLimit {
-    limit
-    remaining
-    used
-    resetAt
-  }
   }
 `;
 
